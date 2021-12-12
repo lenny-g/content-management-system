4709a92929a6efae78f9a346780feb5120d01010
lenny-g <leanne_gallag@live.co.uk>
2021-12-12 15:32:50 +0000

    Added  inProgress formula

--- src/index.js
+++ src/index.js
@@ -10,7 +10,44 @@ const start = async () => {
     password: process.envDB_PASSWORD || "Password123!!",
     database: process.envDB_NAME || "company_db",
   });
+
+  await db.start();
+
+  let inProgress = true;
+
+  while (inProgress) {
+    const { chosenAction } = await inquirer.prompt(actionQuestions);
+    if (chosenAction === "viewEmployee") {
+      console.log("viewEmployees");
+    }
+
+    if (chosenAction === "addEmployee") {
+    }
+
+    if (chosenAction === "updateEmployeeDepartment") {
+      console.log("updateEmployeeDepartment");
+    }
+
+    if (chosenAction === "viewRoles") {
+      console.log("viewRoles");
+    }
+
+    if (chosenAction === "addRoles") {
+      console.log("addRoles");
+    }
+
+    if (chosenAction === "viewDepartments") {
+      console.log("viewDepartments");
+    }
+
+    if (chosenAction === "addDepartment") {
+      console.log("addDepartment");
+    }
+
+    if (chosenAction === "exit") {
+      console.log("exit");
+    }
+  }
 };
 
-await db.start();
 start();
