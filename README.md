bfe2205b591fbe3f88d727fbff407fc1c42ec9ed
lenny-g <leanne_gallag@live.co.uk>
2021-12-12 21:18:22 +0000

    Added utils functions

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# content-management-system-
-build a command-line application from scratch to manage a company's employee database, using Node.js, Inquirer, and MySQL.
+# Content Management System
+
+## Description
+
+The objective of this project was to build a command-line application from scratch to manage a company's employee database, using Node.js, Inquirer, and MySQL.
+
+## Installation
+
+#### The steps and packages to install this project are:
+
+```
+- clone repository
+- npm init -y
+- npm run start
+```
+
+## Usage
+
+#### This section contains a video link showcasing the usage of this application:
+
+Click [here](https://drive.google.com/file/d/1LS230KAc2vBRuz3z-qoaRytaNtiIxyTs/view?usp=sharing) to see a video demonstration of the application
--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
 const inquirer = require("inquirer");
+const {
+  generateDepartmentChoices,
+  generateRoleChoices,
+  generateManagerChoices,
+  generateEmployeeChoices,
+} = require("./utils/utils");
+
+const title = `
+
+███████╗███╗   ███╗██████╗ ██╗      ██████╗ ██╗   ██╗███████╗███████╗
+██╔════╝████╗ ████║██╔══██╗██║     ██╔═══██╗╚██╗ ██╔╝██╔════╝██╔════╝
+█████╗  ██╔████╔██║██████╔╝██║     ██║   ██║ ╚████╔╝ █████╗  █████╗  
+██╔══╝  ██║╚██╔╝██║██╔═══╝ ██║     ██║   ██║  ╚██╔╝  ██╔══╝  ██╔══╝  
+███████╗██║ ╚═╝ ██║██║     ███████╗╚██████╔╝   ██║   ███████╗███████╗
+╚══════╝╚═╝     ╚═╝╚═╝     ╚══════╝ ╚═════╝    ╚═╝   ╚══════╝╚══════╝
+████████╗██████╗  █████╗  ██████╗██╗  ██╗███████╗██████╗ 
+╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗
+   ██║   ██████╔╝███████║██║     █████╔╝ █████╗  ██████╔╝
+   ██║   ██╔══██╗██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗
+   ██║   ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║
+   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+`;
 
 const Db = require("./utils/Db");
 const actionQuestions = require("./utils/questions");
 
+const db = new Db({
+  host: process.envDB_HOST || "localhost",
+  user: process.envDB_USER || "root",
+  password: process.envDB_PASSWORD || "Password123!!",
+  database: process.envDB_NAME || "company_db",
+});
+
 const start = async () => {
-  const db = new Db({
-    host: process.envDB_HOST || "localhost",
-    user: process.envDB_USER || "root",
-    password: process.envDB_PASSWORD || "Password123!!",
-    database: process.envDB_NAME || "company_db",
-  });
+  console.log(title);
 
   await db.start();
 
@@ -26,7 +50,41 @@ const start = async () => {
     }
 
     if (chosenAction === "addEmployee") {
-      console.log("addEmployee");
+      const role = await db.query("SELECT * FROM jobRole");
+      const employee = await db.query("SELECT * FROM employee");
+
+      const employeeQuestions = [
+        {
+          type: "input",
+          message: "Please enter employee's first name:",
+          name: "firstName",
+        },
+        {
+          type: "input",
+          message: "Please enter employee's last name:",
+          name: "lastName",
+        },
+        {
+          type: "list",
+          message: "Please select a role:",
+          name: "jobRoleId",
+          choices: generateRoleChoices(role),
+        },
+        {
+          type: "list",
+          message: "Please select a Manager:",
+          name: "managerId",
+          choices: generateManagerChoices(employee),
+        },
+      ];
+
+      const { jobRoleId, firstName, lastName, managerId } =
+        await inquirer.prompt(employeeQuestions);
+
+      await db.query(
+        `INSERT INTO employee (firstName, lastName, jobRoleId, managerId) VALUES("${firstName}", "${lastName}", ${jobRoleId}, ${managerId})`
+      );
+      console.log(`Added ${firstName} ${lastName} to the database.`);
     }
 
     if (chosenAction === "updateEmployeeRole") {
@@ -55,14 +113,12 @@ const start = async () => {
         `UPDATE employee SET jobRoleId = ${jobRoleId} WHERE id = ${id}`
       );
       console.log(`Employee Role has been updated`);
-    };
     }
 
     if (chosenAction === "viewRoles") {
       const roles =
         await db.query(`SELECT jobRole.title, department.name, jobRole.salary FROM jobRole JOIN department ON jobRole.departmentId = department.id ORDER BY department.name;
-
-      `);
+ `);
       console.table(roles);
     }
 
@@ -112,13 +168,26 @@ const start = async () => {
     }
 
     if (chosenAction === "addDepartment") {
-      console.log("addDepartment");
+      const departmentQuestions = [
+        {
+          type: "input",
+          message: "Please enter new department:",
+          name: "newDepartment",
+        },
+      ];
+
+      const { newDepartment } = await inquirer.prompt(departmentQuestions);
+
+      await db.query(
+        `INSERT INTO department (name) VALUES("${newDepartment}")`
+      );
+      console.log(`Added ${newDepartment} to the database`);
     }
 
     if (chosenAction === "exit") {
-      console.log("exit");
+      inProgress = false;
+      process.exit(0);
     }
   }
 };
-
 start();
